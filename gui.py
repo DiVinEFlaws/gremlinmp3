17ab2b0bcaf85cb18d469b98c5799623779ca268
DiVinEFlaws <redlcamino@gmail.com>
2025-11-04 16:16:22 -0600

    testing branch commit message

--- gui.py
+++ gui.py
@@ -4,10 +4,19 @@ def main():
     root = tk.Tk()
     root.title("File Navigator")
     root.geometry("600x400")
-
+    #initalize all the elements of the gui here
     # --- your widgets and layout will go here ---
 
     root.mainloop()
+
+def moveup():
+    print("navigating up")
+def movedown():
+    print("navigating down")
+def upDirectory():
+    print("moving up a directory")
+def enterDirectory():
+    print("entering a directory or executing a file")
 
 if __name__ == "__main__":
     print("Starting the GUI application...")
