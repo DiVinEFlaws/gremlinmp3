2668aa6c441e96799f55f6e3ef35ae09f58319fe
DiVinEFlaws <redlcamino@gmail.com>
2025-11-04 15:54:35 -0600

    Initial commit

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("File Navigator")
+    root.geometry("600x400")
+
+    # --- your widgets and layout will go here ---
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
